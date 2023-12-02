8a1a63743e32814b847e74549b80e133f925918d
John <johnnkonge2020@gmail.com>
2023-12-02 20:53:45 +0300

    deleted some components

--- travel/src/App.js
+++ travel/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Routess from './routes';
+
 import './App.css';
-import NavBar from './NavBav'
+
 function App() {
  return (
     <div className="App">
-      <NavBar/>
-      <h1>Hello there</h1>
-      <Routess />
+     
+      <h1>Hello there,its coding night. Brace yourself!!</h1>
+      
     </div>
   );
 }
